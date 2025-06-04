45104f7c1c205c7c1ee50a91ffa23772729b8da2
Rafhael <rafharezendesouza@gmail.com>
2025-06-03 21:34:06 -0400

    Json Validation Done, everything working

--- APIEbeer.Controllers/JsonController.cs
+++ APIEbeer.Controllers/JsonController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using APIEbeer.Shared.ViewModels;
+using APIEbeer.Services.Json;
 
 
 namespace APIEbeer.Controllers
 {
     [ApiController]
-    public class JsonController : ControllerBase
+    public class JsonController (IJsonService jsonService): Controller
     {
+        private readonly IJsonService _jsonService = jsonService;
         // Route to receive and validate menuJson
         // POST: api/menu
         [Route("api/menu")]
@@ -14,9 +16,21 @@ namespace APIEbeer.Controllers
         public IActionResult ValidateMenuJson([FromBody] JsonViewModel model)
         {
             // Process the menu JSON here
+            if (model == null)
+            {
+                return BadRequest("O objeto JSON não pode ser nulo.");
+            }
+
+            var (IsValid, ErrorMessage) = _jsonService.ValidateJsonStructure(model);
+
+            if (!IsValid) {
+                return BadRequest(ErrorMessage ?? "Erro de validação desconhecido.");
+            }
             // After processing, return sucess response
             // Change to index view later
             return Ok(model);
         }
     }
+
+
 }
