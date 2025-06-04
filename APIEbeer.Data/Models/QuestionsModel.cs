550753a5d60705e8a38056edb0f2ee0e6fdb5684
João Víctor DS <diassouzajoao2@gmail.com>
2025-06-04 12:37:38 -0400

    Standard questions about characteristics were created

---
+++ APIEbeer.Data/Models/QuestionsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace APIEbeer.Data.Models
+{
+    public class QuestionsModel
+    {
+        public string BitterAndSweet { get; set; } = "Você prefere amargo ou doce?";
+        public string AlcoholContent { get; set; } = "Qual o teor alcoólico que você prefere?";
+        public string Flavor { get; set; } = "Qual o sabor que você prefere?";
+        public string Intencity { get; set; } = "Qual a intensidade que você prefere?";
+        public string Texture { get; set; } = "Qual a textura que você prefere?";
+    }
+}
